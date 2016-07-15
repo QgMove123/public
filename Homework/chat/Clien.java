7fe52b85103ab348e836dbf3ed1c848561751a40
xilong <839351516@qq.com>
2016-07-15 17:34:46 +0800

    zhuxilong

---
+++ Homework/chat/Clien.java
@@ -0,0 +1,49 @@
+package clien;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+class ThreadTest extends Thread{
+	public BufferedReader reader;
+	Socket st;
+	ThreadTest(Socket socket){
+		st = socket;
+	}
+	public void run(){
+		try{
+			while(true){
+				reader = new BufferedReader(new InputStreamReader(st
+						.getInputStream())); // 实例化BufferedReader对象
+				if(reader.ready()) System.out.println(reader.readLine());
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
+public class Clien {
+	String str;
+	public PrintWriter writer;
+	Socket socket;
+	public void connect(){
+		try{
+			if(writer!=null) writer.close();
+			if(socket!=null)socket.close();
+			socket = new Socket("127.0.0.1",8998);
+			Scanner scan = new Scanner(System.in);
+			new ThreadTest(socket).start();
+			while(true){
+				str = scan.nextLine();
+				writer = new PrintWriter(socket.getOutputStream(),true);
+				writer.println(str);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args){
+		Clien clien = new Clien();
+		clien.connect();
+	}
+}
