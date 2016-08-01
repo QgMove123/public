143fd1a8f7a04503b2303b2d7ce3c2f16f39dfc8
xusuisheng <913904049@qq.com>
2016-08-01 23:52:23 +0800

    完成GET、POST方法

---
+++ QGYun/app/src/main/java/com/example/ricco/myinterface/JsonUtil.java
@@ -0,0 +1,18 @@
+package com.example.ricco.myinterface;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JsonUtil {
+	public static String toJson(Object obj){
+		GsonBuilder gb  = new GsonBuilder();
+		Gson gson = gb.create();
+		return gson.toJson(obj);
+	}
+	
+	public static <T> T toObject(String json, Class<T> clazz){
+		GsonBuilder gb = new GsonBuilder();
+		Gson gson = gb.create();
+		return gson.fromJson(json, clazz);
+	}
+}
--- QGYun/app/src/main/java/com/example/ricco/qgyun/MainActivity.java
+++ QGYun/app/src/main/java/com/example/ricco/qgyun/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.ricco.qgyun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
