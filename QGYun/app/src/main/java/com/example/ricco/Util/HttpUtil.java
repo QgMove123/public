6339faa71aadf14f6fa33f6412272a3e558cce1e
StartWhitWzk <841482429@qq.com>
2016-08-02 19:24:45 +0800

    complete Login and register

--- QGYun/app/src/main/java/com/example/ricco/Util/HttpUtil.java
+++ QGYun/app/src/main/java/com/example/ricco/Util/HttpUtil.java
@@ -52,7 +52,6 @@ public class HttpUtil {
                 }
             }
         }.start();
-
     }
 
     public static void getPic(final String url,final CallbackListener listener){
@@ -88,52 +87,4 @@ public class HttpUtil {
         }.start();
 
     }
-
-
-
-//    public static void post(final String url,final String params, final CallbackListener listener){
-//        new Thread(){
-//            @Override
-//            public void run() {
-//                HttpURLConnection conn = null;
-//                BufferedReader br = null;
-//                PrintWriter pw = null;
-//                try {
-//                    URL httpUrl = new URL(url);
-//                    conn = (HttpURLConnection)httpUrl.openConnection();
-//                    conn.setRequestMethod("POST");
-//                    conn.setConnectTimeout(8000);
-//                    conn.setReadTimeout(8000);
-//                    conn.setDoOutput(true);
-//                    conn.setDoInput(true);
-//                    //发送请求参数
-//                    pw = new PrintWriter(conn.getOutputStream());
-//                    pw.print(params);
-//                     //读取URL的响应
-//                    br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//                    StringBuilder result = new StringBuilder();
-//                    String line;
-//                    while((line = br.readLine())!=null) {
-//                        result.append(line);
-//                    }
-//                    if(listener != null){
-//                        //回调onFinish()方法
-//                        listener.onFinish(result.toString());
-//                    }
-//                } catch (IOException e) {
-//                    //回调onError()方法
-//                    listener.onError(e);
-//                } finally {
-//                    try {
-//                        pw.close();
-//                        br.close();
-//                        conn.disconnect();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//        }.start();
-//
-//    }
 }
