eb0a812d64fb42aaf449760c021b8b568e5365c6
chenyi <1581660518@qq.com>
2016-08-13 16:24:34 +0800

    chenyi

---
+++ QGZone/app/src/main/java/com/example/ricco/entity/UserModel.java
@@ -0,0 +1,50 @@
+package com.example.ricco.entity;
+
+/**
+ * Created by chenyi on 2016/8/13.
+ */
+public class UserModel {
+    private int userId;//账号
+    private String userName;//昵称
+    private String password;//密码
+    private int userSecretId;//密保编号
+    private String userSecretAnswer;//密保答案
+    private String userImage;//照片
+
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getUserSecretId() {
+        return userSecretId;
+    }
+    public void setUserSecretId(int userSecretId) {
+        this.userSecretId = userSecretId;
+    }
+    public String getUserSecretAnswer() {
+        return userSecretAnswer;
+    }
+    public void setUserSecretAnswer(String userSecretAnswer) {
+        this.userSecretAnswer = userSecretAnswer;
+    }
+    public String getUserImage() {
+        return userImage;
+    }
+    public void setUserImage(String userImage) {
+        this.userImage = userImage;
+    }
+}
